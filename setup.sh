6723503b950695c2d0053c727f9e48c8cfc9bd79
Isaac Wasserman <isaacrwasserman@gmail.com>
2024-07-22 18:31:50 -0400

    smarter

--- setup.sh
+++ setup.sh
@@ -8,22 +8,31 @@ script_dir=$(dirname "$(realpath "$0")")
 cd "$script_dir" || exit
 
 # Backup existing directories and files
-mv .git .git_distro
-mv .github .github_distro
-mv .gitignore .gitignore_distro
+# mv .git .git_distro
+# mv .github .github_distro
+# mv .gitignore .gitignore_distro
 
 # Clone the repository and move its contents
 git clone https://github.com/comfyanonymous/ComfyUI
-mv ComfyUI/* .
+find ComfyUI -type f -exec bash -c '
+  for file; do
+    dest="./$(basename "$file")"
+    if [ -e "$dest" ]; then
+      echo "File $dest already exists. Skipping."
+    else
+      mv "$file" "$dest"
+    fi
+  done
+' bash {} +
 rm -rf ComfyUI
 
 # Clean up old git files
 rm -rf .git*
 
 # Restore original directories and files
-mv .git_distro .git
-mv .github_distro .github
-mv .gitignore_distro .gitignore
+# mv .git_distro .git
+# mv .github_distro .github
+# mv .gitignore_distro .gitignore
 
 # Clone repositories listed in custom_node_repo_urls.txt
 target_dir="custom_nodes"
@@ -43,7 +52,7 @@ pip install -r requirements.txt
 sed -i 's/^import manager_core as core/from . import manager_core as core/' custom_nodes/ComfyUI-Manager/glob/manager_server.py
 
 # Add lines after if __name__ == "__main__": in main.py
-sed -i '/if __name__ == "__main__":/a     import os\n    os.system("python install_models.py")' main.py
+sed -i '/if __name__ == "__main__":/a\n     import os\n    os.system("python install_models.py")' main.py
 
 # Return to the original directory
 cd "$original_dir" || exit
